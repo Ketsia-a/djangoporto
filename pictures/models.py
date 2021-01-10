b7bb92b9e8b1fceb75ea069af5bc7ab6303a0a81
Ketsia-a <iraketsia2001@gmail.com>
2021-01-11 01:30:58 +0200

    added class methods to the classes

--- pictures/models.py
+++ pictures/models.py
@@ -1,22 +1,76 @@
 from django.db import models
 
 # Create your models here.
-class Image(models.Model):
-    name = models.CharField(max_length = 50)
-    description = models.TextField()
-    location = models.ForeignKey(Location,on_delete=models.CASCADE)
-    category = models.ForeignKey(Category,on_delete=models.CASCADE)
-    def __str__(self):
-        return self.name
 
 class Location(models.Model):
-    name = models.CharField(max_length = 50)
+    name = models.CharField(max_length = 30)
 
     def __str__(self):
        return self.name
+     
+    def save_location(self):
+       self.save()   
+    
+    @classmethod
+    def get_locations(cls):
+       locations = Location.objects.all()
+       return locations
+    
+
+    @classmethod
+    def update_location(cls, pk, value):
+       cls.objects.filter(pk=pk).update(image=value)
+    
+
+    def delete_location(self):
+       self.delete()
+
 
 class Category(models.Model):
     name = models.CharField(max_length = 50)
-
+    
     def __str__(self):
        return self.name
+    
+
+    def save_category(self):
+       self.save()
+    
+
+    def delete_category(self):
+       self.delete()
+
+class Image(models.Model):
+    name = models.CharField(max_length = 50)
+    description = models.TextField()
+    location = models.ForeignKey(Location,on_delete =models.SET_NULL,null =True)
+    category = models.ForeignKey(Category,on_delete =models.SET_NULL,null =True)
+    pub_date = models.DateTimeField(auto_now_add =True,null=True)
+    image = models.ImageField(upload_to = 'pictures/',null=True)
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()
+    
+    def delete_image(self):
+        self.delete()    
+
+    @classmethod
+    def update_image(cls, pk, value):
+        cls.objects.filter(pk =pk).update(image=value)
+
+    @classmethod
+    def get_image_by_pk(cls, pk):
+        cls.objects.filter(pk =pk).all()
+        return image 
+
+    @classmethod
+    def search_image(cls, category):
+        image = cls.objects.filter(category__name__icontains=category)    
+        return image
+
+    @classmethod
+    def filter_by_location(cls, location):
+        image = cls.objects.filter(location__name=location).all()
+        return image    

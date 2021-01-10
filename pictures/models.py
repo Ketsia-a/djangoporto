3a06b99cbfacee35d6aa57e76b7d327acc16ec2d
Ketsia-a <iraketsia2001@gmail.com>
2021-01-10 04:23:55 +0200

    creating  models

--- pictures/models.py
+++ pictures/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    name = models.CharField(max_length = 50)
+    description = models.TextField()
+    location = models.ForeignKey(Location,on_delete=models.CASCADE)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+
+class Location(models.Model):
+    name = models.CharField(max_length = 50)
+
+    def __str__(self):
+       return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length = 50)
+
+    def __str__(self):
+       return self.name

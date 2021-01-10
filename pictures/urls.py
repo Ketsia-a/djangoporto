48c5e4aac10a818f5c28598bfe6db21ebd2dd6a9
Ketsia-a <iraketsia2001@gmail.com>
2021-01-11 01:26:40 +0200

    added a path to the homepage search and location. and the media

---
+++ pictures/urls.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url
+from django.urls import path
+from . import views
+
+urlpatterns=[
+   path('',views.homepage,name = 'homepage'),
+   path('location/<location>',views.location,name = 'location'),
+   path('search',views.search_image,name = 'search_image')
+]
+if settings.DEBUG:
+   urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+

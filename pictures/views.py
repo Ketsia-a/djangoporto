accb06a0bb98074849c71f072f906820529f69a7
Ketsia-a <iraketsia2001@gmail.com>
2021-01-11 08:39:45 +0200

    removed an undersore inlocation

--- pictures/views.py
+++ pictures/views.py
@@ -1,34 +1,36 @@
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse
 from .models import Image,Location,Category
 
 # Create your views here.
 
 def homepage(request):
-    image = Image.objects.all()
+    images = Image.objects.all()
     locations = Location.get_locations()
     category = Category.objects.all()
-    context = {'image':image, 'locations': locations, 'category':category}
+    context = {'images':images, 'locations': locations, 'category':category}
 
     return render(request, 'homepage.html', context )
 
 def location(request, location):
     images = Image.filter_by_location(location)
     locations = Location.get_locations()
-    category = Category.objects.all()
-    context = {'images':images, 'locations': locations, 'category':category}
+    context = {'images':images, 'locations': locations}
 
     return render(request, 'location.html', context)
 
 def search_image(request):
     locations = Location.get_locations()
-    category1 = Category.objects.all()
 
     if 'searchimage' in request.GET and request.GET["searchimage"]:
         category = request.GET.get("searchimage")
         images = Image.search_by_category(category)
- 
+        context = {'images':images, 'locations': locations}
+
         return render(request, 'search.html', {"images":images, 'locations': locations, 'category1':category1})
 
     else:
-        return render(request, 'search.html', {'locations': locations,'category1':category1})
+        flash = 'Your search is empty'
+        context = {'locations': locations, 'flash':flash}
+
+        return render(request, 'search.html', context)
